9a52aab8c92206640063bd863133a84b14f06ddb
Craig Gumbley <craiggumbley@gmail.com>
2020-12-29 16:03:55 +0000

    move helpers to private function

--- src/MESH.Api.Client/Extensions/ServiceCollectionExtensions.cs
+++ src/MESH.Api.Client/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,10 @@
 using MESH.Api.Client.Authentication;
 using MESH.Api.Client.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MESH.Api.Client.Extensions
 {
@@ -24,13 +27,34 @@ namespace MESH.Api.Client.Extensions
                     var handler = new HttpClientHandler();
 
                     handler.ClientCertificateOptions = ClientCertificateOption.Manual;
-                    handler.ClientCertificates.Add(CertificateHelper.GetCertificate(configuration.CertificateThumbprint));
+                    handler.ClientCertificates.Add(GetCertificate(configuration.CertificateThumbprint));
                     handler.ServerCertificateCustomValidationCallback = (m, c, ch, e) => true;
 
                     return handler;
                 });
 
             return services;
+        }
+
+        private static X509Certificate2 GetCertificate(string thumbprint)
+        {
+            using (X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+            {
+                certStore.Open(OpenFlags.ReadOnly);
+
+                X509Certificate2Collection certCollection = certStore.Certificates.Find(
+                                            X509FindType.FindByThumbprint,
+                                            thumbprint.ToUpper(),
+                                            false);
+
+                // Get the first cert with the thumbprint
+                X509Certificate2 cert = (X509Certificate2)certCollection.OfType<X509Certificate>().FirstOrDefault();
+
+                if (cert is null)
+                    throw new Exception($"Certificate with thumbprint {thumbprint} was not found");
+
+                return cert;
+            }
         }
     }
 }
